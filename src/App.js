3c5670d2a5f442a55657c4d662041acca16e9bec
github username <github email address>
2020-11-03 16:19:46 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,82 @@
 import React, { Component, Fragment } from 'react';
-import Header from './components/Header'
-import PizzaForm from './components/PizzaForm'
-import PizzaList from './containers/PizzaList'
+import Header from './components/Header';
+import PizzaForm from './components/PizzaForm';
+import PizzaList from './containers/PizzaList';
 class App extends Component {
+  state = {
+    pizzas: [],
+    editPizza: {
+      id: 0,
+      size: '',
+      topping: '',
+      vegetarian: false
+    }
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pizzas')
+      .then(res => res.json())
+      .then(pizzas => {
+        // console.log(pizzas);
+        this.setState({ pizzas });
+      });
+  }
+  updatePizza = pizza => {
+    // console.log('pizza');
+    // let check = pizza.vegetarian ? 'Vegetarian' : 'Not Vegetarian';
+    this.setState({
+      editPizza: pizza
+    });
+  };
+
+  handleChange = i => {
+    if (i.name === 'topping') {
+      this.setState({
+        editPizza: { ...this.state.editPizza, topping: i.value }
+      });
+    } else if (i.name === 'size') {
+      this.setState({
+        editPizza: { ...this.state.editPizza, size: i.value }
+      });
+    } else if (i.name === 'vegetarian') {
+      let val = i.value === 'true' ? true : false;
+      this.setState({
+        editPizza: { ...this.state.editPizza, vegetarian: val }
+      });
+    }
+    console.log(this.state.editPizza);
+  };
+
+  handleSubmit = editPizza => {
+    fetch(`http://localhost:3000/pizzas/${editPizza.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(editPizza)
+    })
+      .then(res => res.json())
+      .then(pizzaObj => {
+        console.log('from server', pizzaObj);
+        this.setState({
+          pizzas: this.state.pizzas.map(pizza =>
+            pizza.id === pizzaObj.id ? pizzaObj : pizza
+          )
+        });
+      });
+  };
+
   render() {
     return (
       <Fragment>
-        <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <Header />
+        <PizzaForm
+          editPizza={this.state.editPizza}
+          checked={this.state.checked}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <PizzaList pizzas={this.state.pizzas} updatePizza={this.updatePizza} />
       </Fragment>
     );
   }
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
-import Pizza from '../components/Pizza'
+import Pizza from '../components/Pizza';
 class PizzaList extends Component {
-
   render() {
     return (
-      <table className="table table-striped">
+      <table className='table table-striped'>
         <thead>
           <tr>
-            <th scope="col">Topping</th>
-            <th scope="col">Size</th>
-            <th scope="col">Vegetarian?</th>
-            <th scope="col">Edit</th>
+            <th scope='col'>Topping</th>
+            <th scope='col'>Size</th>
+            <th scope='col'>Vegetarian?</th>
+            <th scope='col'>Edit</th>
           </tr>
         </thead>
         <tbody>
-          {
-            //render Pizza here
-          }
+          {this.props.pizzas.map(pizza => {
+            return (
+              <Pizza
+                pizza={pizza}
+                key={pizza.id}
+                updatePizza={this.props.updatePizza}
+              />
+            );
+          })}
         </tbody>
       </table>
     );
   }
-
 }
 
 export default PizzaList;
